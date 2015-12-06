b1cf4f9d13a5c0a8d26b33ed20bb5a526f9c1749
Thilina Buddhika <thilinamb@gmail.com>
2015-12-05 19:16:11 -0700

    Adding the initial version of the dashboard in the vc.

--- dashboard/node/hello.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var fs = require('fs')
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'content-type': 'text/plain'});
-  res.end('Hello Node!\n');
-}).listen(8124);
-
-console.log('Server running on 8124');
